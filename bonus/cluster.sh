56cffc3c0f0d94e82a8719d7d9441b648518bdc8
isadri <abkadriissam@gmail.com>
2026-09-24 13:46:31 +0000

    feat(script): make script accept cluster name argument

--- bonus/cluster.sh
+++ bonus/cluster.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-CLUSTER_NAME="iot"
+CLUSTER_NAME="$2"
 
 if [[ "$1" == "create" ]]; then
     if [[ ! -d .venv ]]; then
@@ -15,13 +15,10 @@ if [[ "$1" == "create" ]]; then
     echo "  Install Ansible and any dependencies"
     pip install -r requirements.txt
 
-    echo "  Install Ansible requirements"
-    ansible-galaxy install -r requirements.yaml
-
     ansible-playbook main.yaml --extra-vars=cluster_name="$CLUSTER_NAME"
 
 elif [[ "$1" == "delete" ]]; then
     k3d cluster delete "$CLUSTER_NAME"
 else
-    echo "Usage: $0 [create|delete]" >&2
+    echo "Usage: $0 [create|delete] <cluster-name>" >&2
 fi
