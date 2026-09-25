30af42cf609b343141f6dea394fc2cf0b1c750e6
isadri <abkadriissam@gmail.com>
2026-09-25 15:38:54 +0000

    refactor: remove Vagrantfile

--- bonus/cluster.sh
+++ bonus/cluster.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-CLUSTER_NAME="$2"
-
 if [[ "$1" == "create" ]]; then
     if [[ ! -d .venv ]]; then
         python3 -m venv .venv
@@ -18,10 +16,10 @@ if [[ "$1" == "create" ]]; then
     echo "  Install Ansible and any dependencies"
     pip install -r requirements.txt
 
-    ansible-playbook main.yaml --extra-vars=cluster_name="$CLUSTER_NAME" --ask-become-pass
+    ansible-playbook main.yaml --ask-become-pass
 
 elif [[ "$1" == "delete" ]]; then
     k3d cluster delete "$CLUSTER_NAME"
 else
-    echo "Usage: $0 [create|delete] <cluster-name>" >&2
+    echo "Usage: $0 [create|delete]" >&2
 fi
